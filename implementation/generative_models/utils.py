major update, modifed all GANs model creation, fixed WGAN-GP gradient penality when using packed inputs, polished and moved files, added __init__ files where needed

implementation/generative_models/utils.py
@@ -1,7 +1,10 @@
 import keras.backend as K
+from datetime import datetime
 from keras import initializers, regularizers, constraints
 from keras.engine import Layer, InputSpec
 import numpy as np
+from keras.layers.merge import _Merge
+from pathlib import Path
 from scipy.misc import imresize
 
 import matplotlib
@@ -19,7 +22,7 @@ def save_samples(generated_data, rows, columns, filenames, is_image):
     if is_image:
         plt.subplots(rows, columns, figsize=(7, 7))
     else:
-        plt.subplots(rows, columns, figsize=(columns*3, rows))
+        plt.subplots(rows, columns, figsize=(columns * 3, rows))
 
     k = 1
     for i in range(rows):
@@ -66,7 +69,7 @@ def save_latent_space(generated_data, grid_size, filenames, is_image):
         for j in range(grid_size):
             plt.subplot(grid_size, grid_size, i * grid_size + j + 1)
             if is_image:
-                plt.imshow((generated_data[i*grid_size + j].reshape(10, 10) + 1.0) / 2.0)
+                plt.imshow((generated_data[i * grid_size + j].reshape(10, 10) + 1.0) / 2.0)
             else:
                 plt.plot((generated_data[i * grid_size + j]).T)
                 plt.ylim(-1, 1)
@@ -79,7 +82,7 @@ def save_latent_space(generated_data, grid_size, filenames, is_image):
     plt.clf()
     plt.close()
 
-    
+
 def split_data(dataset, timesteps):
     D = dataset.shape[1]
     if D < timesteps:
@@ -121,6 +124,25 @@ def wasserstein_loss(y_true, y_pred):
     return K.mean(y_true * y_pred)
 
 
+def gradient_penalty_loss(y_true, y_pred, averaged_samples, gradient_penalty_weight):
+    gradients = K.gradients(y_pred, averaged_samples)[0]
+    gradients_sqr = K.square(gradients)
+    gradients_sqr_sum = K.sum(gradients_sqr, axis=np.arange(1, len(gradients_sqr.shape)))
+    gradient_l2_norm = K.sqrt(gradients_sqr_sum)
+    gradient_penalty = gradient_penalty_weight * K.square(1 - gradient_l2_norm)
+    return K.mean(gradient_penalty)
+
+
+class RandomWeightedAverage(_Merge):
+    def __init__(self, batch_size, **kwargs):
+        super().__init__(**kwargs)
+        self._batch_size = batch_size
+
+    def _merge_function(self, inputs):
+        weights = K.random_uniform((self._batch_size, 1))
+        return (weights * inputs[0]) + ((1 - weights) * inputs[1])
+
+
 class MinibatchDiscrimination(Layer):
     """Concatenates to each sample information about how different the input
     features for that sample are from features of other samples in the same
@@ -156,11 +178,11 @@ class MinibatchDiscrimination(Layer):
                                      shape=(None, input_dim))]
 
         self.W = self.add_weight(shape=(self.nb_kernels, input_dim, self.kernel_dim),
-            initializer=self.init,
-            name='kernel',
-            regularizer=self.W_regularizer,
-            trainable=True,
-            constraint=self.W_constraint)
+                                 initializer=self.init,
+                                 name='kernel',
+                                 regularizer=self.W_regularizer,
+                                 trainable=True,
+                                 constraint=self.W_constraint)
 
         # Set built to true.
         super(MinibatchDiscrimination, self).build(input_shape)
@@ -188,3 +210,22 @@ class MinibatchDiscrimination(Layer):
                   'input_dim': self.input_dim}
         base_config = super(MinibatchDiscrimination, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
+
+
+def generate_run_dir():
+    root_path = Path('outputs')
+    if not root_path.exists():
+        root_path.mkdir()
+
+    current_datetime = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+    run_dir = root_path / current_datetime
+    img_dir = run_dir / 'img'
+    model_dir = run_dir / 'models'
+    generated_datesets_dir = run_dir / 'generated_datasets'
+
+    img_dir.mkdir(parents=True)
+    model_dir.mkdir(parents=True)
+    generated_datesets_dir.mkdir(parents=True)
+
+    return run_dir, img_dir, model_dir, generated_datesets_dir

implementation/generative_models/wgan/wgan.py
@@ -1,6 +1,6 @@
 import pickle
 
-import utils
+from implementation.generative_models import utils
 from keras import Model
 from keras.layers import *
 from keras.optimizers import RMSprop
@@ -8,9 +8,10 @@ from keras.initializers import RandomNormal
 
 
 class WGAN:
-    def __init__(self, timesteps, latent_dim, packing_degree, run_dir, img_dir, model_dir, generated_datesets_dir):
+    def __init__(self, timesteps, latent_dim, packing_degree, batch_size, run_dir, img_dir, model_dir, generated_datesets_dir):
         self._timesteps = timesteps
         self._latent_dim = latent_dim
+        self._batch_size = batch_size
         self._run_dir = run_dir
         self._img_dir = img_dir
         self._model_dir = model_dir
@@ -26,71 +27,109 @@ class WGAN:
 
     def build_models(self, generator_lr, critic_lr):
         self._generator = self._build_generator()
-
         self._critic = self._build_critic()
-        self._critic.compile(loss=utils.wasserstein_loss, optimizer=RMSprop(critic_lr))
 
-        z = Input((self._latent_dim,))
-        supporting_inputs = Input((self._timesteps, self._packing_degree))
+        self._generator_model = self._build_generator_model(generator_lr)
+        self._critic_model = self._build_critic_model(critic_lr)
 
-        fake = self._generator(z)
+        return self._generator, self._critic
 
+    def _build_generator_model(self, generator_lr):
+        utils.set_model_trainable(self._generator, True)
         utils.set_model_trainable(self._critic, False)
-        # valid = self._critic(fake)
-        valid = self._critic([fake, supporting_inputs])
 
-        self._gan = Model([z, supporting_inputs], valid, 'WGAN')
-        # self._gan = Model(z, valid, 'GAN')
+        noise_samples = Input((self._latent_dim,))
+        supporting_noise_samples = Input((self._latent_dim, self._packing_degree))
 
-        self._gan.compile(
-            loss=utils.wasserstein_loss,
-            optimizer=RMSprop(generator_lr))
+        reshaped_supporting_noise_samples = Lambda(lambda x: K.reshape(x, (self._batch_size * self._packing_degree, self._latent_dim)))(supporting_noise_samples)
+        generated_samples = self._generator(noise_samples)
+        generated_samples = Lambda(lambda x: K.reshape(x, (self._batch_size, self._timesteps, 1)))(
+            generated_samples)
+        supporting_generated_samples = self._generator(reshaped_supporting_noise_samples)
+        supporting_generated_samples = Lambda(lambda x: K.reshape(x, (self._batch_size, self._timesteps, self._packing_degree)))(
+            supporting_generated_samples)
 
-        return self._gan, self._generator, self._critic
+        merged_generated_samples = Lambda(lambda x: K.concatenate(x, -1))([generated_samples, supporting_generated_samples])
+
+        generated_criticized = self._critic(merged_generated_samples)
+
+        generator_model = Model([noise_samples, supporting_noise_samples], generated_criticized, 'generator_model')
+        generator_model.compile(loss=utils.wasserstein_loss, optimizer=RMSprop(generator_lr))
+
+        return generator_model
+
+    def _build_critic_model(self, critic_lr):
+        utils.set_model_trainable(self._generator, True)
+        utils.set_model_trainable(self._critic, False)
+
+        noise_samples = Input((self._latent_dim,))
+        real_samples = Input((self._timesteps,))
+        supporting_noise_samples = Input((self._latent_dim, self._packing_degree))
+        supporting_real_samples = Input((self._timesteps, self._packing_degree))
+
+        reshaped_supporting_noise_samples = Lambda(lambda x: K.reshape(x, (self._batch_size * self._packing_degree, self._latent_dim)))(supporting_noise_samples)
+        generated_samples = self._generator(noise_samples)
+        generated_supporting_samples = self._generator(reshaped_supporting_noise_samples)
+
+        expanded_generated_samples = Lambda(lambda x: K.reshape(x, (self._batch_size, self._timesteps, 1)))(generated_samples)
+        expanded_generated_supporting_samples = Lambda(
+            lambda x: K.reshape(x, (self._batch_size, self._timesteps, self._packing_degree)))(generated_supporting_samples)
+        merged_generated_samples = Lambda(lambda x: K.concatenate(x, -1))(
+            [expanded_generated_samples, expanded_generated_supporting_samples])
+
+        generated_criticized = self._critic(merged_generated_samples)
+
+        expanded_real_samples = Lambda(lambda x: K.reshape(x, (self._batch_size, self._timesteps, 1)))(real_samples)
+        merged_real_samples = Lambda(lambda x: K.concatenate(x, -1))([expanded_real_samples, supporting_real_samples])
+
+        real_criticized = self._critic(merged_real_samples)
+
+        critic_model = Model([real_samples, supporting_real_samples, noise_samples, supporting_noise_samples],
+                             [real_criticized, generated_criticized], 'critic_model')
+        critic_model.compile(loss=[utils.wasserstein_loss, utils.wasserstein_loss], optimizer=RMSprop(critic_lr))
+
+        return critic_model
 
     def _build_generator(self):
         generator_inputs = Input((self._latent_dim,))
         generated = generator_inputs
 
-        if self._latent_dim != 12:
-            generated = Dense(12)(generated)
+        if self._latent_dim != 15:
+            generated = Dense(15, kernel_initializer=self._weights_initializer)(generated)
             generated = BatchNormalization()(generated)
-            generated = Activation('relu')(generated)
+            generated = LeakyReLU(0.2)(generated)
 
         generated = Lambda(lambda x: K.expand_dims(x))(generated)
 
-        generated = UpSampling1D(2)(generated)
         generated = Conv1D(64, 3, padding='same', kernel_initializer=self._weights_initializer)(generated)
         generated = BatchNormalization()(generated)
-        generated = Activation('relu')(generated)
-
+        generated = LeakyReLU(0.2)(generated)
         generated = UpSampling1D(2)(generated)
+
         generated = Conv1D(32, 3, padding='same', kernel_initializer=self._weights_initializer)(generated)
         generated = BatchNormalization()(generated)
-        generated = Activation('relu')(generated)
-
+        generated = LeakyReLU(0.2)(generated)
         generated = UpSampling1D(2)(generated)
+
         generated = Conv1D(16, 3, padding='same', kernel_initializer=self._weights_initializer)(generated)
         generated = BatchNormalization()(generated)
-        generated = Activation('relu')(generated)
-        generated = Conv1D(1, 1, padding='same', kernel_initializer=self._weights_initializer)(generated)
+        generated = LeakyReLU(0.2)(generated)
+        generated = UpSampling1D(2)(generated)
+
+        generated = Conv1D(1, 3, padding='same', kernel_initializer=self._weights_initializer)(generated)
         generated = BatchNormalization()(generated)
-        generated = Activation('relu')(generated)
+        generated = LeakyReLU(0.2)(generated)
 
         generated = Lambda(lambda x: K.squeeze(x, -1))(generated)
-        generated = Dense(self._timesteps, activation='tanh')(generated)
+
+        generated = Dense(self._timesteps, activation='tanh', kernel_initializer=self._weights_initializer)(generated)
 
         generator = Model(generator_inputs, generated, 'generator')
         return generator
 
     def _build_critic(self):
-        critic_inputs = Input((self._timesteps,))
-        supporting_inputs = Input((self._timesteps, self._packing_degree))
-
+        critic_inputs = Input((self._timesteps, self._packing_degree + 1))
         criticized = critic_inputs
-
-        criticized = Lambda(lambda x: K.expand_dims(x))(criticized)
-        criticized = Concatenate(-1)([criticized, supporting_inputs])
 
         criticized = Conv1D(16, 3, padding='same', kernel_initializer=self._weights_initializer)(criticized)
         criticized = BatchNormalization()(criticized)
@@ -108,55 +147,53 @@ class WGAN:
         criticized = MaxPooling1D(2, padding='same')(criticized)
 
         criticized = Flatten()(criticized)
-        criticized = Dense(1)(criticized)
 
-        critic = Model([critic_inputs, supporting_inputs], criticized, 'critic')
+        criticized = Dense(15, kernel_initializer=self._weights_initializer)(criticized)
+        criticized = BatchNormalization()(criticized)
+        criticized = LeakyReLU(0.2)(criticized)
+
+        criticized = Dense(1, kernel_initializer=self._weights_initializer)(criticized)
+
+        critic = Model(critic_inputs, criticized, 'critic')
 
         return critic
 
-    def train(self, batch_size, epochs, n_generator, n_critic, dataset, clip_value,
+    def train(self, epochs, n_generator, n_critic, dataset, clip_value,
               img_frequency, loss_frequency, latent_space_frequency, model_save_frequency, dataset_generation_frequency,
               dataset_generation_size):
-        half_batch = int(batch_size / 2)
+
+        ones = np.ones((self._batch_size, 1))
+        neg_ones = -ones
 
         while self._epoch < epochs:
             self._epoch += 1
             critic_losses = []
             for _ in range(n_critic):
-                indexes = np.random.randint(0, dataset.shape[0], (half_batch, self._packing_degree + 1))
-                batch_transactions = dataset[indexes[:, 0]].reshape(half_batch, self._timesteps)
-                supporting_transactions = dataset[indexes[:, 1:]].reshape(half_batch, self._timesteps, self._packing_degree)
+                indexes = np.random.randint(0, dataset.shape[0], (self._batch_size, self._packing_degree + 1))
+                batch_transactions = dataset[indexes[:, 0]].reshape(self._batch_size, self._timesteps)
+                supporting_transactions = dataset[indexes[:, 1:]].reshape(self._batch_size, self._timesteps, self._packing_degree)
 
-                noise = np.random.normal(0, 1, (half_batch * (self._packing_degree + 1), self._latent_dim))
-                generated_transactions = self._generator.predict(noise)
-                supporting_generated_transactions = generated_transactions[half_batch:].reshape(half_batch, self._timesteps, self._packing_degree)
-                generated_transactions = generated_transactions[:half_batch].reshape(half_batch, self._timesteps)
+                noise = np.random.normal(0, 1, (self._batch_size, self._latent_dim))
+                supporting_noise = np.random.normal(0, 1, (self._batch_size, self._latent_dim, self._packing_degree))
 
-                critic_loss_real = self._critic.train_on_batch([batch_transactions, supporting_transactions], -np.ones((half_batch, 1)))
-                critic_loss_fake = self._critic.train_on_batch([generated_transactions, supporting_generated_transactions], np.ones((half_batch, 1)))
-                # critic_loss_real = self._critic.train_on_batch(batch_transactions, -np.ones((half_batch, 1)))
-                # critic_loss_fake = self._critic.train_on_batch(generated_transactions, np.ones((half_batch, 1)))
-
-                critic_losses.append(0.5 * np.add(critic_loss_real, critic_loss_fake))
+                critic_loss = self._critic_model.train_on_batch([batch_transactions, supporting_transactions, noise, supporting_noise],
+                                                          [ones, neg_ones])[0]
+                critic_losses.append(critic_loss)
 
                 utils.clip_weights(self._critic, clip_value)
             critic_loss = np.mean(critic_losses)
 
             generator_losses = []
             for _ in range(n_generator):
-                noise = np.random.normal(0, 1, (batch_size, self._latent_dim))
-                if self._packing_degree > 0:
-                    supporting_noise = np.random.normal(0, 1, (batch_size * self._packing_degree, self._latent_dim))
-                    supporting_generated_transactions = self._generator.predict(supporting_noise).reshape(batch_size, self._timesteps, self._packing_degree)
-                else:
-                    supporting_generated_transactions = np.empty((batch_size, self._timesteps, 0))
+                noise = np.random.normal(0, 1, (self._batch_size, self._latent_dim))
+                supporting_noise = np.random.normal(0, 1, (self._batch_size, self._latent_dim, self._packing_degree))
 
-                generator_losses.append(self._gan.train_on_batch([noise, supporting_generated_transactions], -np.ones((batch_size, 1))))
+                generator_losses.append(self._generator_model.train_on_batch([noise, supporting_noise], ones))
 
             generator_loss = np.mean(generator_losses)
 
-            generator_loss = 1 - generator_loss
-            critic_loss = 1 - critic_loss
+            generator_loss = float(-generator_loss)
+            critic_loss = float(-critic_loss)
 
             self._losses[0].append(generator_loss)
             self._losses[1].append(critic_loss)
@@ -192,7 +229,7 @@ class WGAN:
         generated_transactions = self._generator.predict(noise)
 
         filenames = [str(self._img_dir / ('%07d.png' % self._epoch)), str(self._img_dir / 'last.png')]
-        utils.save_samples(generated_transactions, rows, columns, filenames, False)
+        utils.save_samples(generated_transactions, rows, columns, filenames, True)
 
     def _save_latent_space(self):
         grid_size = 6
@@ -206,7 +243,7 @@ class WGAN:
         generated_data = self._generator.predict(latent_space_inputs)
 
         filenames = [str(self._img_dir / 'latent_space.png')]
-        utils.save_latent_space(generated_data, grid_size, filenames, False)
+        utils.save_latent_space(generated_data, grid_size, filenames, True)
 
     def _save_losses(self):
         utils.save_losses(self._losses, str(self._img_dir / 'losses.png'))